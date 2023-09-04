68f0af4d0986df0927878b32ba4786eed77c97d2
Quinta707 <saraieqp30@gmail.com>
2023-09-04 10:04:49 -0600

    eaeaeaeae

--- simexpro-app/lib/screens/login_screen.dart
+++ simexpro-app/lib/screens/login_screen.dart
@@ -31,14 +31,14 @@ Future<void> fetchData(BuildContext context, String username, String password) a
   );
   if (response.statusCode == 200) {
     Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => HomeScreen(),
+        context,
+        MaterialPageRoute(
+          builder: (context) => NavBarRoots(),
     ));
   } else {
     print('a');
     CherryToast.error(
-      title: Text('El usuario o la contraseña son incorrectos',
+      title: Text('El usuario o contraseña son incorrectos',
            style: TextStyle(color: Color.fromARGB(255, 226, 226, 226))),
       borderRadius: 0,
     ).show(context);
@@ -78,122 +78,105 @@ class _loginScreenState extends State<loginScreen> {
               ),
               SizedBox(height: 10),
               Padding(
-                padding: const EdgeInsets.all(12),
-                child: TextField(
-                   onChanged: (value) {
-                    setState(() {
-                      username = value; 
-                    });
-                  },
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    label: Text("Usuario"),
-                    prefixIcon: Icon(Icons.person),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12),
-                child: TextField(
-                  onChanged: (value) {
-                    setState(() {
-                      password = value; // Actualiza la contraseña cuando cambia el texto
-                    });
-                  },
-                  obscureText: passToggle ? true : false,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    label: Text("Contraseña"),
-                    prefixIcon: Icon(Icons.lock),
-                    suffixIcon: InkWell(
-                      onTap: () {
-                        if (passToggle == true) {
-                          passToggle = false;
-                        } else {
-                          passToggle = true;
-                        }
-                        setState(() {});
-                      },
-                      child: passToggle
-                          ? Icon(CupertinoIcons.eye_slash_fill)
-                          : Icon(CupertinoIcons.eye_fill),
+                  padding: const EdgeInsets.all(12),
+                  child: TextField(
+                    onChanged: (value) {
+                      setState(() {
+                        username = value;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text("Usuario"),
+                      prefixIcon: Icon(Icons.person),
+                      contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 10), // Personaliza el tamaño
                     ),
                   ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: TextField(
+                    onChanged: (value) {
+                      setState(() {
+                        password = value;
+                      });
+                    },
+                    obscureText: passToggle ? true : false,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text("Contraseña"),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: InkWell(
+                        onTap: () {
+                          if (passToggle == true) {
+                            passToggle = false;
+                          } else {
+                            passToggle = true;
+                          }
+                          setState(() {});
+                        },
+                        child: passToggle
+                            ? Icon(CupertinoIcons.eye_slash_fill)
+                            : Icon(CupertinoIcons.eye_fill),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 10), // Personaliza el tamaño
+                    ),
+                  ),
+                ),
               SizedBox(height: 20),
               Padding(
-                padding: const EdgeInsets.all(15),
-                child: InkWell(
-                  // onTap: () {
-                  //   Navigator.push(
-                  //       context,
-                  //       MaterialPageRoute(
-                  //         builder: (context) => NavBarRoots(),
-                  //       ));
-                  // },
-                  onTap: () {
-                    if (username.isNotEmpty && password.isNotEmpty) {
-                      fetchData(context, username, password);
-                    } else {
-
-                      CherryToast.warning(
-                        title: Text('Llene los campos correctamente',
-                             style: TextStyle(color: Color.fromARGB(255, 226, 226, 226))),
-                        borderRadius: 0,
-                      ).show(context);
-                    //   CherryToast.warning(
-                    //   title: Text('Advertencia',
-                    //         style: TextStyle(color: Color(0xffFC9F00)),),
-                    //   enableIconAnimation: false,
-                    //   displayTitle: true,
-                    //   description: Text('Complete los campos correctamente',
-                    //   style: TextStyle(color: Colors.white),),
-                    //   animationType: AnimationType.fromRight,
-                    //   animationDuration: Duration(milliseconds: 1000),
-                    //   autoDismiss: true,
-                    // ).show(context);
-                    }
-                  },
-
-                  child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 15),
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(87, 69, 223, 1),
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black12,
-                          blurRadius: 4,
-                          spreadRadius: 2,
-                        ),
-                      ],
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Iniciar sesión",
-                        style: TextStyle(
-                          fontSize: 23,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                  padding: const EdgeInsets.all(15),
+                  child: InkWell(
+                    onTap: () {
+                      if (username.isNotEmpty && password.isNotEmpty) {
+                        fetchData(context, username, password);
+                      } else {
+                        CherryToast.warning(
+                          title: Text('Llene los campos correctamente',
+                              style: TextStyle(color: Color.fromARGB(255, 226, 226, 226))),
+                          borderRadius: 0,
+                        ).show(context);
+                      }
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20), // Personaliza el tamaño
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(87, 69, 223, 1),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 4,
+                            spreadRadius: 2,
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Iniciar sesión",
+                          style: TextStyle(
+                            fontSize: 18, // Modifica el tamaño de la fuente
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
               SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+             Row(
+                mainAxisAlignment: MainAxisAlignment.end, // Alinea a la derecha
                 children: [
                   TextButton(
                     onPressed: () {
                       Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => SignUpScreen(),
-                          ));
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignUpScreen(),
+                        ),
+                      );
                     },
                     child: Text(
                       "Recuperar contraseña",
