a0837a6ea42a3bb04df1f794200a1a0646c93698
Quinta707 <saraieqp30@gmail.com>
2023-09-27 11:04:28 -0600

    si pudiera comenzar a ser la mitad de lo que crees de mi

--- simexpro-app/lib/api.dart
+++ simexpro-app/lib/api.dart
@@ -1,7 +1,7 @@
 
 //String apiUrl= 'https://practicaacademia.somee.com/api/';
 
-//String apiUrl= 'https://localhost:44380/api/';
-String apiUrl= 'https://practicaacademia.somee.com/api/';
+String apiUrl= 'https://localhost:44380/api/';
+//String apiUrl= 'https://practicaacademia.somee.com/api/';
 
 String apiKey = '4b567cb1c6b24b51ab55248f8e66e5cc';
