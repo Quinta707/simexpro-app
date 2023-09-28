10a3e5bc9b514cb76db59fafccf1e1cce97b901e
Quinta707 <saraieqp30@gmail.com>
2023-09-28 16:11:00 -0600

    i hate the ending myself

--- simexpro-app/lib/widgets/profile_widget.dart
+++ simexpro-app/lib/widgets/profile_widget.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:simexpro/api.dart';
 import 'package:simexpro/toastconfig/toastconfig.dart';
+import 'package:http/http.dart' as http;
+
 
 class ProfileWidget extends StatelessWidget {
   final String imagePath;
@@ -32,21 +36,50 @@ class ProfileWidget extends StatelessWidget {
   }
 
   Future<void> CambiarFoto(context) async {
-    CherryToast.info(title: Text('hola', style: TextStyle(color: Colors.white),)).show(context);
+    CherryToast.info(
+      title: Text('hola', style: TextStyle(color: Colors.white))
+    ).show(context);
+  }
+
+  Future<void> subirImagen(
+    BuildContext context, String img) async {
+  final cuerpo = {''};
+  final jsoncuerpo = jsonEncode(cuerpo);
+  final response = await http.post(
+    Uri.parse('https://api.imgbb.com/1/upload'),
+    headers: {
+      'XApiKey': '7e4e4920016a49b1dfc06d5af4e9ffc3',
+      'Content-Type': 'application/json',
+    },
+    body: jsoncuerpo,
+  );
+}
+  
+  Future<void> _selectImage() async {
+    // final picker = ImagePicker();
+    // final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    // if (pickedFile != null) {
+    //   final path = pickedFile.path;
+    //   final bytes = await File(path).readAsBytes();
+    //   final img.Image? image = img.decodeImage(bytes);
+    //   print(image);
+    // }
   }
 
   Widget buildImage(context) {
-    final image = NetworkImage(imagePath);
+    final imagen = NetworkImage(imagePath);
 
     return ClipOval(
       child: Material(
         color: Colors.transparent,
         child: Ink.image(
-          image: image,
+          image: imagen,
           fit: BoxFit.cover,
           width: 170,
           height: 170,
-          child: InkWell(onTap: () {CambiarFoto(context);}),
+          child: InkWell(
+            onTap: () {}
+          ),
         ),
       ),
     );
@@ -64,7 +97,9 @@ class ProfileWidget extends StatelessWidget {
             color: Colors.white,
             size: 20,
           ),
-          onTap: () {CambiarFoto(context);},
+          onTap: () {
+              _selectImage();
+          },
           ) 
         ),
       );
@@ -77,7 +112,7 @@ class ProfileWidget extends StatelessWidget {
       ClipOval(
         child: Container(
           padding: EdgeInsets.all(all),
-          color: color,
+          color: Color.fromRGBO(99, 74, 158, 1),
           child: child,
         ),
       );
