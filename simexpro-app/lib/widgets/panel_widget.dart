916db7bcca7281ae0380165ef96f2c5468834896
aless65 <140451736+SIMEXPRO@users.noreply.github.com>
2023-09-28 23:12:31 -0600

    ya funcional la mariconada del encabezado

--- simexpro-app/lib/widgets/panel_widget.dart
+++ simexpro-app/lib/widgets/panel_widget.dart
@@ -55,7 +55,7 @@ class PanelWidget extends StatelessWidget{
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         // Text("hola"),
-
+    
         //Search bar
         Center(
           child: SizedBox(
@@ -101,214 +101,144 @@ class PanelWidget extends StatelessWidget{
           ),
         ),
         //End search bar
-
+    
         const SizedBox(height: 27),
-
-        //Items
-        Container(
-        height: 135,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.deepPurple.withOpacity(0.5),
-              spreadRadius: 3,
-              blurRadius: 10,
-              offset: const Offset(0, 3),
-            )
-          ],
-        ),
-        child: Material(
-          borderRadius: BorderRadius.circular(15),
-          child: InkWell(
-            splashColor: Colors.grey,
-            borderRadius: BorderRadius.circular(15),
-            onTap: (){
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const ItemTrackingScreen(),
-                )
-              );
-            },
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-              Row(
+    
+        ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: detalles.length,
+          itemBuilder: (BuildContext context, int index){
+            //Items
+            return SingleChildScrollView(
+              child: Column(
                 children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.53, // 60% of the width
-                    child: const Padding(
-                      padding: EdgeInsets.only(left: 15, top: 23),
-                      child: Text(
-                        "Código de ítem: 314",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        maxLines: 1,
-                        overflow: TextOverflow.fade,
-                        softWrap: false,
-                      ),
+                  Container(
+                    height: 135,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.deepPurple.withOpacity(0.5),
+                          spreadRadius: 3,
+                          blurRadius: 10,
+                          offset: const Offset(0, 3),
+                        )
+                      ],
                     ),
-                  ),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 18, top: 26),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(15),
+                      child: InkWell(
+                        splashColor: Colors.grey,
+                        borderRadius: BorderRadius.circular(15),
+                        onTap: (){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const ItemTrackingScreen(),
+                            )
+                          );
+                        },
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Container(
-                              decoration: const BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.green,
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.53, // 60% of the width
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 15, top: 23),
+                                  child: Text(
+                                    "Código de ítem: ${detalles[index]["code_Id"]}",
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    maxLines: 1,
+                                    overflow: TextOverflow.fade,
+                                    softWrap: false,
+                                  ),
+                                ),
                               ),
-                              width: 20.0 / 2,
-                              height: 20.0 / 2,
-                            ),
-                            //Dot and text separator
-                            const SizedBox(width: 6),
-                            const Flexible(
+                              Expanded(
+                                child: Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(right: 18, top: 26),
+                                    child: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Container(
+                                          decoration: const BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Colors.green,
+                                          ),
+                                          width: 20.0 / 2,
+                                          height: 20.0 / 2,
+                                        ),
+                                        //Dot and text separator
+                                        const SizedBox(width: 6),
+                                        Flexible(
+                                          child: Text(
+                                            // softWrap: false,
+                                            detalles[index]["proc_Descripcion"].toUpperCase(),
+                                            // overflow: TextOverflow.clip,
+                                            style: const TextStyle(fontSize: 11),
+                                            textAlign: TextAlign.end,
+                                            maxLines: 1,
+                                            overflow: TextOverflow.fade,
+                                            softWrap: false,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                      
+                      
+                      
+                            Padding(
+                              padding: const EdgeInsets.only(left: 15, top: 16, right: 18),
                               child: Text(
                                 // softWrap: false,
-                                "CORTE DE TELA",
-                                // overflow: TextOverflow.clip,
-                                style: TextStyle(fontSize: 11),
-                                textAlign: TextAlign.end,
+                                "ESTILO: ${detalles[index]["esti_Descripcion"]}, TALLA: ${detalles[index]["tall_Nombre"]}",
+                                // overflow: TextOverflow.ellipsis,
                                 maxLines: 1,
                                 overflow: TextOverflow.fade,
                                 softWrap: false,
+                                style: const TextStyle(fontSize: 13),
                               ),
                             ),
+                      
+                            Padding(
+                              padding: const EdgeInsets.only(left: 15, top: 6, right: 18),
+                              child: Text(
+                                "COLOR: ANARANJADO, CANTIDAD: ${detalles[index]["code_CantidadPrenda"]}",
+                                maxLines: 1,
+                                overflow: TextOverflow.fade,
+                                softWrap: false,
+                                style: const TextStyle(fontSize: 13),
+                              ),
+                            ),
+                      
                           ],
                         ),
                       ),
                     ),
                   ),
+                
+                    const SizedBox(height: 27),
                 ],
               ),
-          
-          
-          
-                const Padding(
-                  padding: EdgeInsets.only(left: 15, top: 16, right: 18),
-                  child: Text(
-                    // softWrap: false,
-                    "ESTILO: Escotada, TALLA: Extra Extra Small",
-                    // overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                    overflow: TextOverflow.fade,
-                    softWrap: false,
-                    style: TextStyle(fontSize: 13),
-                  ),
-                ),
-          
-                const Padding(
-                  padding: EdgeInsets.only(left: 15, top: 6, right: 18),
-                  child: Text(
-                    "COLOR: ANARANJADO, CANTIDAD: 50",
-                    maxLines: 1,
-                    overflow: TextOverflow.fade,
-                    softWrap: false,
-                    style: TextStyle(fontSize: 13),
-                  ),
-                ),
-          
-              ],
-            ),
-          ),
+            );
+          },
         ),
-      ),
-
-
-        const SizedBox(height: 27),
-
-        //Items
-        Container(
-          height: 135,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: [
-              BoxShadow(
-                // color:  Color.fromARGB(255, 119, 86, 195).withOpacity(0.5),
-                color:  Colors.deepPurple.withOpacity(0.5),
-                spreadRadius: 3,
-                blurRadius: 10,
-                offset: const Offset(0, 3),
-              )
-            ],
-          ),
-          child: Stack(
-            // alignment: Alignment.center,
-            children: [
-              const Positioned(
-                top: 23,
-                left: 15,
-                child: Text(
-                  "Código de ítem: 297",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Positioned(
-                width: MediaQuery.of(context).size.width * 0.25,
-                top: 26,
-                right: 17,
-                  child: Align(
-                    alignment: AlignmentDirectional.centerEnd,
-                    child: Wrap(
-                      children: [
-                        Container(
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.purple,
-                          ),
-                        width: 20.0 / 2,
-                        height: 20.0 / 2,
-                        ),
-                        const SizedBox(width: 6),
-                        ConstrainedBox(
-                          constraints: BoxConstraints(
-                            maxWidth: MediaQuery.of(context).size.width * 0.21,
-                          ),
-                          child: const Text(
-                            "ESTAMPADO",
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(fontSize: 11),
-                            textAlign: TextAlign.end,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-              ),
-              const Positioned(
-                top: 65,
-                left: 15,
-                child: Text(
-                  "ESTILO: Escotada, TALLA: Extra Extra Small",
-                  style: TextStyle(fontSize: 13),
-                )
-              ),
-              const Positioned(
-                top: 87,
-                left: 15,
-                child: Text(
-                  "CANTIDAD: 12",
-                  style: TextStyle(fontSize: 13),
-                )
-              ),
-            ],
-          ),
-        )
+        
       ],
     ),
   );
